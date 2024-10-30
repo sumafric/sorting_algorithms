68af15d629a6989af385c4e8cc15910bb20ecea3
Abel Sum <abelsum1330@gmail.com>
2024-10-29 21:08:13 -0700

    Selection sort

--- sort.h
+++ sort.h
@@ -18,17 +18,11 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swap(listint_t *a, listint_t *b);
-void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void swap1(int *a, int *b, int *array, size_t size);
-size_t lomuto_partition(int *array, int low, int high);
-void quicksort(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif
